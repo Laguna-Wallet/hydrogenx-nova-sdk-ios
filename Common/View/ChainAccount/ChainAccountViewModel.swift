1a1397c51f2322a1cc99d70a8cbce71b12848844
Oleh Hastov <8984253+oleh-hastov@users.noreply.github.com>
2022-06-22 14:15:04 +0300

    [HX-413] Cutting views, presenters, wireframes, viewfactories

--- Common/View/ChainAccount/ChainAccountViewModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SubstrateSdk
-
-struct ChainAccountViewModel {
-    let networkName: String
-    let address: String
-    let accountIcon: DrawableIcon
-    let networkIconViewModel: ImageViewModelProtocol?
-}
