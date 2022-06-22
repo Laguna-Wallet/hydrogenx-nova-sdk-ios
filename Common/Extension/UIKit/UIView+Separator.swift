ce54285ba2888995e5adf53cc2fe9b8def4cde37
Oleh Hastov <8984253+oleh-hastov@users.noreply.github.com>
2022-06-22 17:13:40 +0300

    [HX-413] Cutting UI related stuff

--- Common/Extension/UIKit/UIView+Separator.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIView {
-    static func createSeparator(color: UIColor? = R.color.colorLightGray()) -> UIView {
-        let view = UIView()
-        view.backgroundColor = color
-        return view
-    }
-}
